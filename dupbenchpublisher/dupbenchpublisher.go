e84765e6547818aa7986a6fced0f6abe1c9b2419
Evan Jones <ej@evanjones.ca>
2019-11-24 11:56:46 -0500

    dupbunchpublisher: "Working" version! (#6)
    Also add Go program to create the topics and subscriptions

--- dupbenchpublisher/dupbenchpublisher.go
+++ dupbenchpublisher/dupbenchpublisher.go
@@ -20,6 +20,8 @@ import (
 // 16 = 128 bits which should make collisions "impossible"
 const goroutineIDLength = 16
 
+const publishWaitInterval = 500
+
 func setTimestampNow(ts *timestamp.Timestamp) {
 	// stupidly over-optimized to avoid allocations (probably unnecessary)
 	t := time.Now()
@@ -27,9 +29,19 @@ func setTimestampNow(ts *timestamp.Timestamp) {
 	ts.Nanos = int32(t.Nanosecond())
 }
 
+func waitForPublish(ctx context.Context, results []*pubsub.PublishResult) {
+	for _, r := range results {
+		_, err := r.Get(ctx)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
 func publisherGoroutine(wg *sync.WaitGroup, topic *pubsub.Topic, idString string, numMessages int) {
 	defer wg.Done()
 	log.Printf("goroutine %s starting; %d numMessages", idString, numMessages)
+
+	ctx := context.Background()
 
 	// benchmarks show this is silghtly faster
 	buf := proto.NewBuffer(nil)
@@ -38,22 +50,32 @@ func publisherGoroutine(wg *sync.WaitGroup, topic *pubsub.Topic, idString string
 		GoroutineId: idString,
 		Created:     ptypes.TimestampNow(),
 	}
+	psMsg := &pubsub.Message{}
+	results := []*pubsub.PublishResult{}
 	for i := 0; i < numMessages; i++ {
 		msg.Sequence = int64(i)
 		setTimestampNow(msg.Created)
-
 		err := buf.Marshal(msg)
 		if err != nil {
 			panic(err)
 		}
+
+		psMsg.Data = buf.Bytes()
+		results = append(results, topic.Publish(ctx, psMsg))
+		if len(results) >= publishWaitInterval {
+			waitForPublish(ctx, results)
+			results = results[:0]
+		}
+
 		buf.Reset()
 	}
+	waitForPublish(ctx, results)
 	log.Printf("goroutine %s exiting", idString)
 }
 
 func main() {
 	projectID := flag.String("projectID", "", "Google Cloud Project to use (empty = default)")
-	topicID := flag.String("topicID", "", "Pubsub topic ID to publish to")
+	topicID := flag.String("topicID", "dupbench", "Pubsub topic ID to publish to")
 	goroutines := flag.Int("goroutines", 5, "Number of goroutines to use for publishing")
 	numMessages := flag.Int("numMessages", 1000, "numMessages each goroutine will publish")
 	flag.Parse()
@@ -81,6 +103,13 @@ func main() {
 		go publisherGoroutine(wg, topic, idString, *numMessages)
 	}
 	wg.Wait()
+
+	// technically probably does not need to be closed since we are exiting
+	topic.Stop()
+	err = pubsubClient.Close()
+	if err != nil {
+		panic(err)
+	}
 	end := time.Now()
 
 	duration := end.Sub(start)
